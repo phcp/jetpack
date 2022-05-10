f85af30b4b3b6402cdf353cf98a29b69c7fc145e
Anna McPhee <anna.mcphee@automattic.com>
2022-05-10 17:48:13 +0100

    change returned data structure to fit RecordMeterBar

--- projects/packages/search/src/dashboard/components/record-meter/lib/record-info.js
+++ projects/packages/search/src/dashboard/components/record-meter/lib/record-info.js
@@ -57,8 +57,12 @@ export default function getRecordInfo(
 		for ( let i = 0; i < numItems; i++ ) {
 			const postTypeDetails = Object.values( postTypeBreakdown )[ i ];
 			const { count, slug: name } = postTypeDetails;
+<<<<<<< HEAD
 
 			chartPostTypeBreakdown.push( {
+=======
+			postTypeBreakdown.push( {
+>>>>>>> 67182a9e0d (change returned data structure to fit RecordMeterBar)
 				data: createData( count, colors[ i ], name ),
 			} );
 			currentCount = currentCount + count;
@@ -72,36 +76,36 @@ export default function getRecordInfo(
 		);
 
 		// push includedItems into the recordInfo
-		for ( const item in postTypeItems.includedItems ) {
-			recordInfo.push( {
-				data: createData(
-					postTypeItems.includedItems[ item ].data.data[ 0 ],
+		for ( const item in PostTypeItems.includedItems ) {
+			recordInfo.push(
+				createData(
+					PostTypeItems.includedItems[ item ].data.count,
 					colors[ item ],
-					postTypeItems.includedItems[ item ].data.label
-				),
-			} );
+					PostTypeItems.includedItems[ item ].data.label
+				)
+			);
 		}
 
 		// populate the 'other' category with combined remaining items and push to end of data array
-		if ( postTypeItems.otherItems.length > 0 ) {
-			recordInfo.push( {
-				data: createData(
-					combineOtherCount( postTypeItems.otherItems ),
+		if ( PostTypeItems.otherItems.length > 0 ) {
+			recordInfo.push(
+				createData(
+					combineOtherCount( PostTypeItems.otherItems ),
 					PALETTE.colors[ 'Gray 30' ],
-					'Other'
-				),
-			} );
+					__( 'other', 'jetpack-search-pkg' )
+				)
+			);
 		}
 
 		// if there is remaining unused space in tier, add filler spacing to chart
-		if ( typeof tierMaximumRecords === 'number' && tierMaximumRecords - currentCount > 0 ) {
-			recordInfo.push( {
-				data: createData(
-					tierMaximumRecords - currentCount,
+		if ( tier - currentCount > 0 ) {
+			recordInfo.push(
+				createData(
+					tier - currentCount,
 					PALETTE.colors[ 'Gray 0' ],
 					__( 'remaining', 'jetpack-search-pkg' )
-				),
-			} );
+				)
+			);
 		}
 	}
 
@@ -128,7 +132,7 @@ export default function getRecordInfo(
  * @returns {object} containing included items with post type and count, and other items, split.
  */
 export function splitUsablePostTypes( postTypeBreakdown, numItems, maxPostTypeCount ) {
-	postTypeBreakdown.sort( ( a, b ) => ( a.data.data[ 0 ] < b.data.data[ 0 ] ? 1 : -1 ) );
+	postTypeBreakdown.sort( ( a, b ) => ( a.data.count[ 0 ] < b.data.count[ 0 ] ? 1 : -1 ) );
 
 	const count = maxPostTypeCount <= numItems ? maxPostTypeCount : numItems;
 
@@ -148,9 +152,8 @@ export function combineOtherCount( otherItems ) {
 	let runningTotal = 0;
 
 	for ( const item in otherItems ) {
-		runningTotal = otherItems[ item ].data.data[ 0 ] + runningTotal;
+		runningTotal = otherItems[ item ].data.count + runningTotal;
 	}
-
 	return runningTotal;
 }
 
@@ -164,7 +167,7 @@ export function combineOtherCount( otherItems ) {
  */
 export function createData( data, color, name ) {
 	return {
-		data: [ data ],
+		count: data,
 		label: name,
 		backgroundColor: color,
 	};
